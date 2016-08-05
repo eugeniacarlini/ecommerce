ed324f0df792c33b68c918507f8042ad3da5a268
Eugenia Carlini <eugenia.carlini@avenida.com>
2016-08-05 11:25:41 -0300

    Actualización del archivo ver perfil

---
+++ verPerfil.php
@@ -0,0 +1,48 @@
+<?php
+	require_once("soporte.php");
+	$usuarioActivo = getUsuarioLogueado();
+
+	$usuarioPerfil = $repositorio->getUserRepository()->getUsuarioById($usuarioActivo->getId());
+
+	$dirname = "uploads/avatars/";
+	$imagen = glob($dirname . "*.*");
+
+	if (estaLogueado())
+	{
+		include("includes/headerLogueado.php");
+	}
+	else
+	{
+		include("includes/headerNoLogueado.php");
+	}
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-offset-4 col-md-4">
+      <h1>Mi perfil</h1>
+      <form class="register-form text-center" action="" method="post" enctype="multipart/form-data">
+        <?php if (!empty($errores)) { ?>
+          <div class="alert alert-danger" role="alert">
+            <ul>
+              <?php foreach ($errores as $error) { ?>
+                <li>
+                  <?php echo $error ?>
+                </li>
+              <?php } ?>
+            </ul>
+          </div>
+        <?php } ?>
+				<img class="img-rounded center-block" src="<?php echo $imagen[0] ?>" alt="" />
+				<input type="file" id="imagen" name="imagen" class="center-block" />
+				<p><?php echo $usuarioPerfil->getNombre() ?></p>
+				<p><?php echo $usuarioPerfil->getApellido() ?></p>
+				<p><?php echo $usuarioPerfil->getMail() ?></p>
+				<a href="editarPerfil.php" title="Editar perfil" class="btn btn-success btn-block">Editar perfil</a>
+				<a href="index.php" title="Volver" class="btn btn-default btn-block">Volver</a>
+      </form>
+    </div>
+  </div>
+</div>
+
+<?php include("includes/footer.php"); ?>
