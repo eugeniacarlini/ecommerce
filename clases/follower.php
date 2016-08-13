d8383dafb390a6ae2a1937400c7dabac0938d0db
Gonzalo Vanni <gonzalovanni@gmail.com>
2016-08-13 20:29:27 -0300

    carga de followers en la base de datos

--- clases/follower.php
+++ clases/follower.php
@@ -1,22 +1,26 @@
 <?php
 require_once 'auth.php';
-require_once 'specsProduct.php';
 
 class Follower
 {
-
+  private $id;
   private $id_follower;
   private $id_following;
 
 
-  function __construct(Array $follower)
+  public function __construct(Array $follower)
   {
-    // $this->id = array_key_exists("id", $follower) ? $follower["id"] : null;
+    $this->id = array_key_exists("id", $follower) ? $follower["id"] : null;
     $auth = Auth::getInstance();
     $usuarioLogueado = $auth->getUsuarioLogueado();
     $this->id_follower = $usuarioLogueado->getId();
-    $this->id_following = $miProducto->getIdUsuario();
+    $this->id_following = $follower['id_following'];
+    // $this->id_following = array_key_exists("id_following", $follower) ? $follower["id_following"] : null;
+  }
 
+  public function getId(){
+
+    return $this->id;
   }
 
   public function getId_Follower()
@@ -29,14 +33,18 @@ class Follower
     return $this->id_following;
   }
 
-  public function setId_Follower()
-  {
-    return $this->id_follower = $id_follower;
+  public function setId($id){
+    return $this->id;
   }
 
-  public function setId_Following()
+  public function setId_Follower($follower)
   {
-    return $this->id_following = $id_following;
+    $this->id_follower = $id_follower;
+  }
+
+  public function setId_Following($following)
+  {
+    $this->id_following = $id_following;
   }
 
 }
--- clases/followerMYSQLrepository.php
+++ clases/followerMYSQLrepository.php
@@ -1,7 +1,6 @@
 <?php
 require_once ("soporte.php");
-require_once ("spectsProduct.php");
-require_once("follower.php?>");
+
 class FollowerMYSQLrepository {
 
 	private $miConexion;
@@ -11,30 +10,46 @@ class FollowerMYSQLrepository {
 		$this->miConexion = $miConexion;
 	}
 
+	public function mostrarFollower($idfollowing)
+	{
+		$stmt = $this->miConexion->prepare("SELECT COUNT(*), idFollowing FROM Followers WHERE `idfollower` = $idfollowing group by idFollowing ");
 
-public function guardarFollower(Follower $follower)
-{
-  if ($follower->getId())
-  {
-    if ($this->getProductoById($follower->getId()))
-    {
-      $stmt = $this->miConexion->prepare("UPDATE user_follow SET follower = :follower, following = :following WHERE id = :id");
-    }
-    else
-    {
-      $stmt = $this->miConexion->prepare("INSERT INTO user_follow (id, follower, following) values (:id, :follower, :following)");
-    }
+		var_dump($stm);exit;
+		$stmt->bindValue(":idFollowing", $idfollowing);
+		$stmt->bindValue(":idFollower", $idfollower);
 
-    $stmt->bindValue(":id", $follower->getId());
+		$stmt->execute();
 
-  }
-  else
-  {
-    $stmt = $this->miConexion->prepare("INSERT INTO user_follow (id, follower, following) values (:id, :follower, :following)");
-  }
+		$seguidores = $stmt->fetch();
+		return $seguidores;
+	}
+
+	public function guardarFollower(Follower $follower)
+	{
+		if ($follower->getId())
+		{
+			if ($this->getUsuarioById($follower->getId()))
+			{
+				$stmt = $this->miConexion->prepare("UPDATE Followers SET idFollower = :idFollower, idFollowing = :idFollowing WHERE id = :id");
+			}else{
+
+				$stmt = $this->miConexion->prepare("INSERT INTO Followers (idFollower, idFollowing) values (:idFollower, :idFollowing)");
+			}
+
+			$stmt->bindValue(":id", $follower->getId());
+
+		}
+		else
+		{
+			$stmt = $this->miConexion->prepare("INSERT INTO Followers (idFollower, idFollowing) values (:idFollower, :idFollowing)");
+		}
 
 
-  $stmt->bindValue(":follower", $follower->get());
-  $stmt->bindValue(":following", $follower->());
-  $stmt->execute();
+		$stmt->bindValue(":idFollower", $follower->getId_Follower());
+		$stmt->bindValue(":idFollowing", $follower->getId_following());
+
+		$stmt->execute();
+
+
+	}
 }
